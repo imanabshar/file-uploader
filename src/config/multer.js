b7bc484c43efc4f405076344e769afef8fb1fc39
imanabshar <imanaabshar@gmail.com>
2026-05-02 23:49:28 +0500

    feat: add validation to file

--- src/config/multer.js
+++ src/config/multer.js
@@ -15,6 +15,32 @@ const storage = new CloudinaryStorage({
   },
 });
 
-const upload = multer({ storage });
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = [
+    'image/jpeg',
+    'image/png',
+    'image/jpg',
+    'application/pdf',
+    'application/msword',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  ];
+
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true); // accept file
+  } else {
+    cb(
+      new Error(
+        'Invalid file type. Only images, PDFs and Word documents are allowed.',
+      ),
+      false,
+    ); // reject file
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 8 * 1024 * 1024 }, //8 MB
+  fileFilter,
+});
 
 export default upload;
